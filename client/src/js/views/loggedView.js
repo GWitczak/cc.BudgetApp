26d1ff0e157431cd0994490bfb8e01ca4e91f4f0
unknown <aleksandra.przytula1995@gmail.com>
2019-09-15 22:37:56 +0200

    logged v1

---
+++ client/src/js/views/loggedView.js
@@ -0,0 +1,42 @@
+import BaseView from './_baseView';
+
+class LoggedView extends BaseView {
+    constructor() {
+        super();
+        this.elStr{};
+}
+    
+
+// 
+
+
+/* <div class="logged">
+ <div class="logged__header">
+   <h2>Witaj Użytkowniku</h2>
+ </div>
+ <div class="logged__container">
+   <div class="logged__accounts">
+     <h3>Twoje konta</h3>
+   </div>
+   <div class="logged__account">
+     <h4>mBank</h4>
+   </div>
+   <div class="logged__account">
+    <h4>Alior</h4>
+  </div>
+  <div class="logged__account">
+    <h4>Gotówka</h4>
+  </div>
+  <div class="logged__add">
+    <button class="logged__button">+</button>
+  </div>
+ </div>
+</div> */
+
+
+    init () {
+            
+    }
+};
+
+export default LoggedView; 
