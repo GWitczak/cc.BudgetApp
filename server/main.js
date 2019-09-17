2daea58b15e6f278a8f48608b40f0ae74af206b6
Paweł <praczykp2705@vp.pl>
2019-09-17 20:50:43 +0200

    Added swagger documenation, adding transactions

--- server/main.js
+++ server/main.js
@@ -1,5 +1,289 @@
+
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+    info: {
+        title: 'My budegt api',
+        version: '1.0.0',
+        description: 'Endpoints to test the user registration routes',
+    },
+    host: 'localhost:8000',
+    basePath: '/',
+    "tags": ["Users", "Transactions", "wallet"],
+    "paths": {
+        "/api/users": {
+            "get": {
+                "tags": ["Users"],
+                "summary": "Returns a list of users.",
+                "produces": [
+                    "application/json"
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                         "schema": {
+                             "type": "object",
+                             "properties": {
+                                 "wallet": {
+                                     "type": "integer",
+                                     "example": []
+                                 },
+                                 "globalBalance": {
+                                     "type": "integer",
+                                     "example": 1200
+                                 },
+                                 "history": {
+                                     "type": "integer",
+                                     "example": []
+                                 },
+                                 "isAdmin": {
+                                     "type": "boolean",
+                                     "example": false
+                                 },
+                                 "_id": {
+                                     "type": "string",
+                                     "example": "5d7bc1c38c31d13d57e63b4d"
+                                 },
+                                 "name": {
+                                     "type": "string",
+                                     "example": "Arthur Dent"
+                                 },
+                                 "email": {
+                                     "type": "string",
+                                     "example": "artur@cl.pl"
+                                 },
+                                 "password": {
+                                     "type": "string",
+                                     "example": "$2b$10$QZvfdJYc1V5gEFhZpONdROGUQr8njg9FXE9rTU85OrLHbi.jttZxe"
+                                 }
+                             }
+                         }
+                    }
+                }
+            },
+            "post": {
+                "tags": ["Users"],
+                "summary": "Creates a new user.",
+                "parameters": [{
+                        
+                }],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "type": "object",
+                            "properties": {
+                                "email": {
+                                    "type": "string",
+                                    "example": "paw@op.pl"
+                                },
+                                "password": {
+                                    "type": "string",
+                                    "example": "qwe123"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        },
+        "/api/transactions": {
+            "get": {
+                "tags": ["Transactions"],
+                "summary": "Returns a list of transactions for acc.",
+                "produces": [
+                    "application/json"
+                ],
+                "parameters": [{
+                        "in": "x-auth-token",
+                }],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                         "schema": {
+                             "type": "object",
+                             "properties": {
+                                 "casrTransaction": {
+                                     "type": "boolean",
+                                     "example": false
+                                 },
+                                 "cardOwner": {
+                                     "type": "string",
+                                     "example": null
+                                 },
+                                 "from": {
+                                     "type": "boolean",
+                                     "example": false
+                                 },
+                                 "_id": {
+                                     "type": "string",
+                                     "example": "5d7bc1c38c31d13d57e63b4d"
+                                 },
+                                 "date": {
+                                     "type": "string",
+                                     "example": "2019-09-15T13:32:56.000"
+                                 },
+                                 "accountType": {
+                                     "type": "string",
+                                     "example": "account"
+                                 },
+                                 "type": {
+                                     "type": "string",
+                                     "example": "inc"
+                                 },
+                                 "title": {
+                                     "type": "string",
+                                     "example": "Eloszka"
+                                 },
+                                 "amount": {
+                                     "type": "integer",
+                                     "example": 120
+                                 },
+                                 "category": {
+                                     "type": "string",
+                                     "example": "food"
+                                 }, "wallet_id": {
+                                     "type": "string",
+                                     "example": "5d7bc1c38c31d13d57e63b4d"
+                                 }
+                             }
+                         }
+                    }
+                }
+            },
+            "post": {
+                "tags": ["Transactions"],
+                "summary": "Creates new Transaction.",
+                "parameters": [{
+                }],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                             "type": "object",
+                             "properties": {
+                                 "casrTransaction": {
+                                     "type": "boolean",
+                                     "example": false
+                                 },
+                                 "cardOwner": {
+                                     "type": "string",
+                                     "example": null
+                                 },
+                                 "from": {
+                                     "type": "boolean",
+                                     "example": false
+                                 },
+                                 "_id": {
+                                     "type": "string",
+                                     "example": "5d7bc1c38c31d13d57e63b4d"
+                                 },
+                                 "date": {
+                                     "type": "string",
+                                     "example": "2019-09-15T13:32:56.000"
+                                 },
+                                 "accountType": {
+                                     "type": "string",
+                                     "example": "account"
+                                 },
+                                 "type": {
+                                     "type": "string",
+                                     "example": "inc"
+                                 },
+                                 "title": {
+                                     "type": "string",
+                                     "example": "Eloszka"
+                                 },
+                                 "amount": {
+                                     "type": "integer",
+                                     "example": 120
+                                 },
+                                 "category": {
+                                     "type": "string",
+                                     "example": "food"
+                                 }, "wallet_id": {
+                                     "type": "string",
+                                     "example": "5d7bc1c38c31d13d57e63b4d"
+                                 }
+                             }
+                         }
+                    }
+                }
+            }
+
+        },
+        "/api/wallet/{id}": {
+            "delete": {
+                "tags": ["wallet"],
+                "summary": "Delete wallet.",
+                "produces": [
+                    "application/json"
+                ],
+                "parameters": [{
+                    "in": "x-auth-token",
+                }],
+                "responses": {
+                    "200": {
+                        "description": "OK"
+                    }
+                }
+            }
+
+        },
+        "/api/wallet/": {
+            "post": {
+                "tags": ["wallet"],
+                "summary": "Creates a new wallet.",
+                "parameters": [{}],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "type": "object",
+                            "properties": {
+                                "type": {
+                                    "type": "string",
+                                    "example": "account"
+                                },
+                                "name": {
+                                    "type": "string",
+                                    "example": "Alior"
+                                },
+                                "balance": {
+                                    "type": "iterer",
+                                    "example": 1200
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+            
+    }
+        
+};
+
+
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 require('./startup/routes')(app);
 require('./startup/db')();
--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -9,14 +9,29 @@ const auth = require('../middleware/auth')
 router.post('/', auth, async (req, res) => {
     let user = await User.findOne({ _id: req.user._id });
     if (!user) return res.status(404).send("Couldn't find user with that id.");
+    
+    let index = await user.wallet.findIndex((x) => {
+        return x._id == req.body.wallet_id;
+    })
+    if (index === -1) return res.status(404).send("Couldn't find wallet with that id.");
+
+    let wallet = user.wallet[index];
+
     try {
-        let transaction = Transaction.create(req.body, res);
-        if (req.body.type === 'exp') user.globalBalance = user.globalBalance - transaction.amount;
+        let transaction = Transaction.create(req, res);
+        if (req.body.type === 'exp') {
+            if (req.body.amount > wallet.balance) {
+                user.globalBalance = user.globalBalance - transaction.amount;
+                wallet.balance = wallet.balance - transaction.amount;
+            } else return res.status(404).send("You don't have enough money to complete payment.");
+        }
         else user.globalBalance = user.globalBalance + transaction.amount;
 
         user.history.push(transaction);
+        user.wallet[index].history.push(transaction);
+        console.log(user.wallet[index]);
         user = await user.save();
-
+        console.log(user);
         res.send(transaction);
     } catch (err) {
         res.status(500).send(err.message);
